cbb42b31695cac905ee4dc5bf87d98c97d3faf54
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-08-02 20:51:55 +0900

    rails g controller pages about contact privacy terms israeltour

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
   
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/privacy'
+  get 'pages/terms'
+  get 'pages/israeltour'
   # rootは最後に記述する
   root 'front#index'
 end
